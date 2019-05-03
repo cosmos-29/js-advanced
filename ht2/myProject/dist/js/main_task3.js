a2601042ac577669c29115b28fa89631252ed7b3
cosmos-29 <a.a.kraskov@gmail.com>
2019-05-03 15:29:56 +0300

    Сделано до конца бургерная.

--- ht2/myProject/dist/js/main_task3.js
+++ ht2/myProject/dist/js/main_task3.js
@@ -1,160 +1,43 @@
 // Создаем класс генерация продуктов;
-class ProductsHamburger {
-    constructor(container = ['.burgers', '.checkOptions'], check =  [false, true]) {
-        this.container = container[0];
-        this.container2 = container[1];
-        this.products = [];
-        this.toppings = [];
-        this.productsPlusTopping = [];
-        this.productsAll = [];
-        this.check = check;
-        this._init();
-    }
-
-    _init() {
-        this._fetchProducts();
-        this._genSummaryItem();
-        this._render();
-
-
-    }
-
-    _fetchProducts() {
-        this.products = [
-            {id: 2, title: 'Big Burger', price: 100, calories: 40, link: null},
-            {id: 1, title: 'Small Burger', price: 50, calories: 20, link: null},
-        ];
-        this.toppings = [
-            {id: 3, title: 'Cheese', price: 10, calories: 20},
-            {id: 4, title: 'Salad', price: 20, calories: 5},
-            {id: 5, title: 'Potatoes', price: 15, calories: 10},
-            {id: 6, title: 'Pepper', price: 15, calories: 0},
-            {id: 8, title: 'Mayonnes', price: 20, calories: 5},
-        ];
-        let counter = 1;
-        for (let product of this.toppings) {
-            counter++;
-            let checkboxd= 'defaultCheck' + counter;
-            product.idOfElement = checkboxd;
-        }
-    }
-
-    _genSummaryItem() {
-        let i = 0;
-        for (let product of this.products) {
-            const prodObj = new ProductItem(product, this.check[i], this.toppings);
-            this.productsPlusTopping.push(prodObj);
-            i++;
-        }
-    }
-    _render() {
-        const block = document.querySelector(this.container);
-        let i = 0;
-        for (let product of this.productsPlusTopping) {
-            const prod = new ProductItemForRender(product, this.check[i]);
-            this.productsAll.push(prod);
-            block.insertAdjacentHTML('beforeend', prod.render());
-            if (product.compare) {
-                const blockForTurn = document.querySelector(this.container2);
-                for (let item of product.link) {
-                    blockForTurn .insertAdjacentHTML('beforeend', `<div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck${item.id - prod.id - 1}">
-                                        <label class="form-check-label" for="defaultCheck1">
-                                            ${item.title}
-                                        </label>
-                                    </div>`)
-                }
-            }
-            i++;
-        }
-    }
-
-}
-class ProductItemForRender {
-    constructor(product, k, img = 'https://placehold.it/370x370') {
-        this.title = product.title;
-        this.price = product.price;
-        this.check = k;
-        this.id = product.id;
-        this.img = img;
-    }
-
-    render() {
-        return `<div class="card mb-3">
-                    <div class="row no-gutters">
-                        <div class="col-md-4"> <img src="${this.img}" class="card-img" alt="..."> </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">${this.title}</h5>
-                                <p class="card-text">${this.price} рублей.</p>
-                                <div class="checkOptions">                             
-
-                                </div>
-                                <button type="button" class="btn btn-outline-danger" onclick="window.location='#'"> Calculate </button>
-                                </div>
-                        </div>
-                    </div>
-                </div>`
+class Param {
+    constructor(element) {
+        this.name = element.value;
+        this.price = +element.dataset['price'];
+        this.calories = +element.dataset['calories'];
     }
 }
-
-class ProductItem {
-    constructor(product, n, k, img = 'https://placehold.it/180x180') {
-        //this.toppings = k;
-        this.title = product.title;
-        this.price = product.price;
-        this.id = product.id;
-        this.link = null;
-        this.img = img;
-        this.compare = n;
-        //this.Checkbox = null;
-        this._returnLink(k);
+class Burger {
+    constructor(size, add, toppings) {
+        this.size = new Param(this._select(size));
+        this.add = new Param(this._select(add));
+        this.toppings = this._getToppings(toppings);
     }
-
-
-    _returnLink(k) {
-        if (this.compare) {
-            this.link = k;
-        }
+    _getToppings(name) {
+        let result = [];
+        this._selectAll(name).forEach(el => result.push(new Param(el)));
+        return result;
     }
-}
-
-const newObject = new ProductsHamburger();
-
-
-//Создаем класс Гамбургерс
-class Hamburger {
-    constructor(size, stuffing) {
-        this.size = size;
-        this.stuffing = stuffing;
+    _select(name){
+        return document.querySelector(`input[name="${name}"]:checked`);
     }
-
-    addTopping(topping) {
-
+    _selectAll(name){
+        return [...document.querySelectorAll(`input[name="${name}"]:checked`)];
     }
-
-    getToppings(topping) {
-
+    _sumPrice() {
+        let result = this.size.price + this.add.price;
+        this.toppings.forEach(el => result += el.price);
+        return result;
     }
-
-    getSize() {
-
+    _sumCalories() {
+        let result = this.size.calories + this.add.calories;
+        this.toppings.forEach(el => result += el.calories);
+        return result;
     }
-
-    getStuffing() {
-
-    }
-
-    calculatePrice() {
-
-    }
-
-    calculateCalories() {
-
+    _showSum(price, calories) {
+        document.querySelector(price).textContent = this._sumPrice();
+        document.querySelector(calories).textContent = this._sumCalories();
     }
 }
-
-//const burgerGen = new Hamburger();
 
 
 // Создаем класс data для отображения в footer;
