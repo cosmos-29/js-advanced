2d56063e8d2f3961ed052759860f8a9f9bfb4ab6
cosmos-29 <a.a.kraskov@gmail.com>
2019-05-14 15:15:05 +0300

    Начал 3-е задание

--- ht4/dist/js/validator.js
+++ ht4/dist/js/validator.js
@@ -1,13 +1,34 @@
 class FormList {
+    constructor() {
+        this.name = '';
+        this.mail = '';
+        this.phone = '';
+        this._init();
+    }
 
-}
-class FormItem {
-    constructor(item) {
-        this.name = item.name;
-        this.check = item.check;
+    _init() {
+        document.querySelector('.form-validate').addEventListener('submit', e => {
+        e.preventDefault();
+        this.filter(document.querySelector('.check-field'));
+    })
     }
-    render() {
-        return
-        //пока неясно что писать;
+
+    filter(value) {
+        this.name = value.querySelector('.name').querySelector('.form-control').value;
+        this.mail = value.querySelector('.mail').querySelector('.form-control').value;
+        this.phone = value.querySelector('.phone').querySelector('.form-control').value;
+        this._validate();
+    }
+    _validate() {
+        //if (this.mail =)
+
     }
 }
+
+let classCheckList = new FormList();
+const array = ['name', 'mail', 'phone'];
+
+const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const
+
+
