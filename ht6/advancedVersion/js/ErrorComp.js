9937e68fa798410782950469d8017c8417793258
cosmos-29 <a.a.kraskov@gmail.com>
2019-05-21 18:12:17 +0300

    3 задание

--- ht6/advancedVersion/js/ErrorComp.js
+++ ht6/advancedVersion/js/ErrorComp.js
@@ -1,17 +1,27 @@
 Vue.component('error', {
     data() {
         return {
-             showProduct: true,
+            showError: false,
+            block: ''
         }
     },
     methods: {
-        errorScan() {
-            if(this.$parent.error) alert("Ошибка!");
+        check() {
+            if(this.$parents.error) {
+                this.showError = true;
+            }
+            if(this.showError) {
+                this.block = "visible"
+            } else this.block = "invisible"
         }
     },
 
-    template: `<div class="alert alert-danger" role="alert">
-                Это уведомление об опасности — check it out!
+    template: `<div class="alert alert-danger" role="alert">  
+                <mess :class="this.block"></mess>             
                </div>`
 
+});
+Vue.component('mess', {
+    props: ['product', 'img'],
+    template: `<p>Ошибка</p>`
 });
